25c3ac8a01c16a1e8479fa57b6cbaf606f26ae46
Surya Prakash <suryaprakash.nnv@gmail.com>
2020-05-20 06:10:22 +0530

    angular-taskmanager-20-05-2020-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AboutComponent } from './admin/about/about.component';
+import { MyProfileComponent } from './admin/my-profile/my-profile.component';
+import { ProjectsComponent } from './admin/projects/projects.component';
+import { UsersComponent } from './admin/users/users.component';
+import { LoginComponent } from './login/login.component';
+import { CanActivateGuardService } from './can-activate-guard.service';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent, canActivate: [CanActivateGuardService] },
+  { path: 'my-profile', component: MyProfileComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TaskManager';
+  constructor(private loginService: LoginService) {}
+  onSearchClick() {
+    console.log(this.loginService.currentUserName);
+  }
 }
